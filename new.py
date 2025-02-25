b9594467c458cea1ad918eb872b3f3a9de84156b
Sharad8546 <sharadsn99@gmail.com>
2025-02-25 10:32:00 +0000

    ss

--- new.py
+++ new.py
@@ -1,9 +1,39 @@
 from flask import Flask
 from flask import render_template
-
+from flask_sqlalchemy import SQLAlchemy
 
 
 app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///library.db'
+
+db=SQLAlchemy(app)
+class User(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(50),nullable=False)
+    user_name=db.Column(db.String(50),unique=True,nullable=False)
+    password=db.Column(db.String(50),nullable=False)
+    DOB=db.Column(db.DateTime,nullable=True)
+    role=db.Column(db.String(20),default="student",nullable=False)
+
+    def __repr__(self):
+        return f'<User{self.User}>'
+class Book(db.Model):
+    Bookname=db.Column(db.String(80),nullable=False)
+    Book_ID=db.Column(db.Integer,primary_key=True)
+    Author=db.Column(db.String(50),nullable=False)
+    category=db.Column(db.String(25),nullable=True)
+    published_year=db.Column(db.Integer,nullable=False)
+    price=db.Column(db.Float,nullable=False)
+    review=db.Column(db.String(150),nullable=True)
+    ratings=db.Column(db.Integer,nullable=True)
+
+class Service(db.Model):
+    s_id=db.Column(db.Integer,primary_key=True)
+    user_id=db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
+    Book_id=db.Column(db.Integer,db.ForeignKey('book.Book_ID'),nullable=False)
+    Issue_date=db.Column(db.DateTime,nullable=False)
+    return_date=db.Column(db.DateTime,nullable=False)
+    status=db.Column(db.String(25),default=False)
 
 @app.route('/')
 def home():
@@ -35,4 +65,6 @@ def add_category():
     return render_template("add_category.html")
 
 if __name__=="__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
